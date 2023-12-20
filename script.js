c4bf0e22b0bf24fff96a365590a0dd89b7e3a422
sreejitasarkar <sreejitasarkar2000@gmail.com>
2023-12-20 23:56:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let b= 'Hey';
+let c= "Hello";
+function firstProg(){
+    let a={
+        name:'Sreejita',
+        dob:982000,
+        place:'Kolkata'
+    }
+    console.log(a);
+}
+firstProg();
+
